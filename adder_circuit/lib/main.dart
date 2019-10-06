9a15951ef4c69fa0ce8d89bc53586c76b1b74c54
Gustavo <gustavo.tatarem@saedigital.com.br>
2019-10-06 15:17:23 -0300

    Created Tab Navigator

--- adder_circuit/lib/main.dart
+++ adder_circuit/lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:adder_circuit/pages/home_page.dart';
+import 'package:adder_circuit/pages/base_converter.dart';
+import 'package:adder_circuit/pages/adder_circuit.dart';
+import 'package:adder_circuit/pages/circuits_page.dart';
+import 'package:adder_circuit/pages/logic_gates.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,12 +10,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Adder Circuit',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return  MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.compare_arrows)),
+                Tab(icon: Icon(Icons.call_split)),
+                Tab(icon: Icon(Icons.add)),
+                Tab(icon: Icon(Icons.grid_on)),
+              ],
+            ),
+            title: Text('Circuito somador'),
+          ),
+          body: TabBarView(
+            children: [
+              BaseConverterPage(),
+              LogicGatesPage(),
+              AdderCircuitPage(),
+              CircuitsPage()
+            ],
+          ),
+        ),
       ),
-      home: HomePage(),
     );
   }
 }
